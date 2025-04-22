f265639f7dcb310ec64cb6ac385671603d83b90f
Sarah Hall <swatson1994@yahoo.com>
2025-04-22 11:23:59 -0400

    add blank models for pcc and combat, get all three classes to show on combat show

--- app/controllers/combat_controller.rb
+++ app/controllers/combat_controller.rb
@@ -4,15 +4,28 @@ class CombatController < ApplicationController
   end
 
   def new
-    
+    @player_characters = PlayerCharacter.all
   end
 
   def create
-    
+    combat = Combat.new
+    combat.save
+     players = params.select {|k,v| v == "1"}.keys
+     players.each do |player_id|
+      pc = PlayerCharactersCombat.new({
+        combat_id: combat.id,
+        player_character_id: player_id
+      })
+      pc.save
+     end
+     redirect_to "/combat/#{combat.id}"
   end
 
   def show
-    
+    @combat = Combat.find(params[:id])
+    @pcc = PlayerCharactersCombat.where(combat_id: params[:id])
+    player_ids = @pcc.map {|pcc| pcc.player_character_id}
+    @players = PlayerCharacter.where(id: player_ids)
   end
 
 end
